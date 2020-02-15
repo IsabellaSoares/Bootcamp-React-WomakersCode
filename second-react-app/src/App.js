81c1419e729c603fe4b95c3579288185191a5bc3
Isabella <isabella.soares.28@gmail.com>
2020-02-15 12:06:32 -0300

    Start second project - Third class

---
+++ second-react-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FormPage from './components/FormPage';
+
+function App() {
+  return (
+    <FormPage/>
+  );
+}
+
+export default App;
---
+++ second-react-app/src/components/FormPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class FormPage extends Component {
+    render () {
+        return (
+
+        );
+    }
+}
