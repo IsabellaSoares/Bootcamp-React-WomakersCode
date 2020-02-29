41550c655f5aef0fcd3fcacf99ea71971728cae1
Isabella <isabella.soares.28@gmail.com>
2020-02-29 16:22:57 -0300

    feat: finish redux flow

--- fourth-react-app-redux/src/App.js
+++ fourth-react-app-redux/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-
-const mapStateToProps = store => ({
-  newValue: store.clickState.newValue
-});
+import { bindActionCreators } from 'redux';
+import { clickButton } from './actions';
 
 class App extends Component {
+  state = {
+    inputValue: ''
+  }
+
+  inputChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+
   render () {
+    const { clickButton, newValue } = this.props;
+
     return (
       <div className="App" style={{ padding: '10px' }}>
-        <input type='text' />
-        <button>Click me!</button>
-        <h1>teste</h1>
+        <input type='text' value={this.state.inputValue} onChange={this.inputChange} />
+        <button onClick={() => clickButton(this.state.inputValue)}>Click me!</button>
+        <h1>{newValue}</h1>
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps)(App);
+const mapStateToProps = store => ({
+  newValue: store.clickState.newValue
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({ clickButton }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- fourth-react-app-redux/src/reducers/clickReducer.js
+++ fourth-react-app-redux/src/reducers/clickReducer.js
@@ -1,12 +1,12 @@
 import { CLICK_UPDATE_VALUE } from '../actions/actionsTypes';
 
 const initialState = {
-    newValue: ''
+    newValue: 'Isa'
 }
 
 export const clickReducer = (state = initialState, action) => {
     switch(action.type) {
-        case 'CLICK_UPDATE_VALUE':
+        case CLICK_UPDATE_VALUE:
             return {
                 ...state,
                 newValue: action.newValue
