a88f69e3b56b1064f03ed1dc2b98ad04feae2514
Isabella <isabella.soares.28@gmail.com>
2020-03-07 12:35:19 -0300

    feat: create MoviesList component

---
+++ studio-ghibli-challenge/src/MoviesList.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Row } from 'antd';
+import MovieCard from './MovieCard';
+
+function MoviesList () {
+    const [list, setList] = useState([]);
+
+    const URL_TO_FETCH = 'https://ghibliapi.herokuapp.com';
+    
+    fetch(URL_TO_FETCH + '/films', {
+        method: 'get'
+    }).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        setList(data);
+    }).catch(function(err) { console.log(err) });
+
+    return (
+        <Row>
+            {list.map((film) => {
+                return(
+                    <MovieCard title={film.title} description={film.description} />
+                );
+            })}
+        </Row>
+    );
+}
+
+export default MoviesList;
