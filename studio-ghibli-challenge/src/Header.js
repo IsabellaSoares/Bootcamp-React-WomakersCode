fbf24ca43bbd16f3eed5626b317268f413dece31
Isabella <isabella.soares.28@gmail.com>
2020-03-07 13:32:48 -0300

    feat: create Header component

---
+++ studio-ghibli-challenge/src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import LogoGhibli from './logo.png';
+
+function Header () {
+    return (<div style={{padding: '20px 0'}}><img src={LogoGhibli} alt="Logotipo Studio Ghibli"/></div>);
+}
+
+export default Header;
