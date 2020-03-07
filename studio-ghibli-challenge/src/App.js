4be461f25835cc8d8d508ff3fef763756b5bb04f
Isabella <isabella.soares.28@gmail.com>
2020-03-07 12:37:03 -0300

    initial setup

--- studio-ghibli-challenge/src/App.js
+++ studio-ghibli-challenge/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './App.css'
+import LogoGhibli from './logo.png';
+import MoviesList from './MoviesList';
 
 function App() {
   return (
     <div className="App">
+      <img src={LogoGhibli} />
+      <MoviesList />
     </div>
   )
 }
