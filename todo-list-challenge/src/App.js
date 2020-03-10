1485f6db5d54a965b35d85f16c9314be54664afd
Isabella <isabella.soares.28@gmail.com>
2020-03-09 22:34:15 -0300

    feat: WIP - todo list

--- todo-list-challenge/src/App.js
+++ todo-list-challenge/src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div style={{padding: 10}}>
         <AddTodo/>
-        {/* <VisibleTodoList/> */}
+        <VisibleTodoList/>
         {/* <Footer/> */}
     </div>
   )
--- todo-list-challenge/src/containers/VisibleTodoList.js
+++ todo-list-challenge/src/containers/VisibleTodoList.js
@@ -2,11 +2,63 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
+import { Button, Row, Col } from 'antd';
+import { Table } from 'antd';
+import { CheckOutlined } from '@ant-design/icons';
 
 
 function VisibleTodoList() {
+  const columns = [
+    {
+      title: 'Todo',
+      dataIndex: 'todo',
+      key: 'todo',
+      align: 'center',
+      width: '80%'
+    },
+    {
+      title: 'Complete!',
+      key: 'action',
+      align: 'center',
+      render: (text, record) => (
+        <span>
+          {/* <a style={{ marginRight: 16 }}>Invite {record.name}</a>
+          <a>Delete</a> */}
+          {/* <Button><CheckCircleOutlined /></Button> */}
+          <Button shape="circle" icon={<CheckOutlined />} size="medium" />
+        </span>
+      ),
+    },
+  ];
+  
+  const data = [
+    {
+      key: '1',
+      todo: 'John Brown'
+    },
+    {
+      key: '2',
+      todo: 'Jim Green'
+    },
+    {
+      key: '3',
+      todo: 'Joe Black'
+    },
+  ];
 
   //componente que mostra as todos adicionadas e cuida da lógica de completar ou não a task
+  return (
+    <>
+      <Row type="flex"
+      style={{ alignItems: "center" }}
+      justify="center">
+        <Col span={12}>
+          <Table columns={columns} dataSource={data} align="center" />
+        </Col>
+      </Row>
+      
+    </>
+  )
   
 }
 
