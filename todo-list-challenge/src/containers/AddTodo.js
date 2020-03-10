fe5d533e8ab681b1b84d7299546161078ffcdeb5
Isabella <isabella.soares.28@gmail.com>
2020-03-09 22:33:49 -0300

    finish form layout

--- todo-list-challenge/src/containers/AddTodo.js
+++ todo-list-challenge/src/containers/AddTodo.js
@@ -1,10 +1,23 @@
 import React, { useRef } from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
-import { Form, Button, Row, Col, Input } from 'antd'
-import { PlusCircleOutlined } from '@ant-design/icons';
+import { Form } from '@ant-design/compatible';
+import '@ant-design/compatible/assets/index.css';
+import { Button, Row, Col, Input } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 function AddTodo() {
+
+    const buttonStyle = {
+        backgroundColor: '#73d13d',
+        borderColor: '#73d13d',
+        color: 'white',
+        float: 'right'
+    }
+
+    const inputStyle = {
+        width: '80%'
+    }
 
     return (
         <>
@@ -12,11 +25,11 @@ function AddTodo() {
             style={{ alignItems: "center" }}
             justify="center">
                 <Col span={12}>
-                    <Form>
-                        <Input type="text" name="name" placeholder="What needs to be done today?" />
-                        <Button type="primary" size='medium'>
-                        <PlusCircleOutlined />Primary
-        </Button>
+                    <Form layout="inline">
+                        <Input type="text" name="name" placeholder="What needs to be done today?" style={inputStyle} />
+                        <Button size='medium' style={buttonStyle}>
+                            <PlusOutlined />Add todo
+                        </Button>
                     </Form>
                 </Col>
             </Row>
