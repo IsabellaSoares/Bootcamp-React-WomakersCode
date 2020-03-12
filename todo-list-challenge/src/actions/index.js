c2bfaf0d6a6e44b73170881c142e00675052319f
Isabella <isabella.soares.28@gmail.com>
2020-03-11 21:05:12 -0300

    feat: add actions

--- todo-list-challenge/src/actions/index.js
+++ todo-list-challenge/src/actions/index.js
@@ -1,15 +1,28 @@
+// Here are the bound action creators
+// They automatically dispatches the actions
+
+// ACTIONS TYPES
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+// ACTION CREATORS
 export const addTodo = text => ({
-  
-})
+  type: ADD_TODO,
+  text
+});
 
 export const setVisibilityFilter = filter => ({
-  
-})
+  type: SET_VISIBILITY_FILTER,
+  filter
+});
 
 export const toggleTodo = id => ({
-  
-})
+  type: TOGGLE_TODO,
+  id
+});
 
+// OTHER CONSTANTS
 export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_COMPLETED: 'SHOW_COMPLETED',
