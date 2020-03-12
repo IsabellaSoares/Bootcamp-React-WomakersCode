81cefa36d62ff5ea1ca01b8a104c4bc166126950
Isabella <isabella.soares.28@gmail.com>
2020-03-11 23:40:33 -0300

    refactor: refactoring containers

--- todo-list-challenge/src/containers/AddTodo.js
+++ todo-list-challenge/src/containers/AddTodo.js
@@ -1,50 +1,32 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
-import '@ant-design/compatible/assets/index.css';
-import { Form, Button, Row, Col, Input } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
 
-function AddTodo ({ addTodo }) {
+let AddTodo = ({ dispatch }) => {
+  let input
 
-    const [newTodo, setNewTodo] = useState('');
-
-    const buttonStyle = {
-        backgroundColor: '#73d13d',
-        borderColor: '#73d13d',
-        color: 'white',
-        float: 'right'
-    }
-
-    const inputStyle = {
-        width: '80%'
-    }
-
-    const onFinish = () => {
-        addTodo(newTodo);
-    };
-
-    return (
-        <>
-            <Row type="flex"
-            style={{ alignItems: "center" }}
-            justify="center">
-                <Col span={12}>
-                    <Form onFinish={onFinish}>
-                        <Form.Item>
-                            <Input 
-                                placeholder="What needs to be done today?"
-                                style={inputStyle}
-                                onChange={e => setNewTodo(e.target.value)} />
-                            <Button size="medium" htmlType="submit" style={buttonStyle}>
-                                <PlusOutlined />Add todo
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </Col>
-            </Row>
-        </>
-    )
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          if (!input.value.trim()) {
+            return
+          }
+          dispatch(addTodo(input.value))
+          input.value = ''
+        }}
+      >
+        <input
+          ref={node => {
+            input = node
+          }}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  )
 }
+AddTodo = connect()(AddTodo)
 
 export default AddTodo
--- todo-list-challenge/src/containers/VisibleTodoList.js
+++ todo-list-challenge/src/containers/VisibleTodoList.js
@@ -1,54 +1,36 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector, connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
-import { Button, Row, Col } from 'antd';
-import { Table } from 'antd';
-import { CheckOutlined } from '@ant-design/icons';
+import { VisibilityFilters } from '../actions';
 
-
-function VisibleTodoList ({ todos, setTodoComplete }) {
-  const columns = [
-    {
-      title: 'Todo',
-      dataIndex: 'todo',
-      key: 'todo',
-      align: 'center',
-      width: '80%',
-      render(text, record) {
-        return {
-          props: {
-            style: { textDecoration: (record['completed'] ? 'line-through' : 'none')},
-          },
-          children: <div>{text}</div>
-        };
-      },
-    },
-    {
-      title: 'Complete!',
-      key: 'action',
-      align: 'center',
-      render: (record) => (
-        <span>
-          <Button shape="circle" icon={<CheckOutlined />} size="medium" onClick={() => setTodoComplete(record.key)} />
-        </span>
-      ),
-    },
-  ];
-  
-  return (
-    <>
-      <Row type="flex"
-      style={{ alignItems: "center" }}
-      justify="center">
-        <Col span={12}>
-          <Table columns={columns} dataSource={todos} align="center" />
-        </Col>
-      </Row>
-      
-    </>
-  )
-  
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(t => t.completed);
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(t => !t.completed);
+    case VisibilityFilters.SHOW_ALL:
+      return todos;
+    default:
+      throw new Error('Unknown filter: ' + filter);
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTodoClick: id => {
+      dispatch(toggleTodo(id))
+    }
+  }
+}
+
+const VisibleTodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList);
 
 export default VisibleTodoList
