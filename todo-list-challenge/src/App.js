762f38a54850181f6bc08e6cee8b28f847cd25d1
Isabella <isabella.soares.28@gmail.com>
2020-03-14 10:34:31 -0300

    refactor: remove Ant's Cols and Rows

--- todo-list-challenge/src/App.js
+++ todo-list-challenge/src/App.js
@@ -2,21 +2,13 @@ import React from 'react';
 import Footer from './components/Footer';
 import AddTodo from './containers/AddTodo';
 import VisibleTodoList from './containers/VisibleTodoList';
-import { Row, Col } from 'antd';
-
-const style = {
-  padding: '15px',
-  height: '100%'
-}
 
 const App = () => (
-	<Row style={style}>
-		<Col span={24}>
-			<AddTodo />
-			<VisibleTodoList />
-			<Footer />
-		</Col>
-	</Row>
+	<>
+		<AddTodo />
+		<VisibleTodoList />
+		<Footer />
+	</>
 );
 
 export default App;
