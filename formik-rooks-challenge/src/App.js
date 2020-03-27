fb1a2eaa34830ea1205fd022f9492a388a8119cc
Isabella <isabella.soares.28@gmail.com>
2020-03-27 19:33:19 -0300

    first version

--- formik-rooks-challenge/src/App.js
+++ formik-rooks-challenge/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from 'formik';
 
-function App() {
+const initialValues = {
+  name: '',
+  email: '',
+  address: {
+    street: '',
+    number: '',
+    city: ''
+  },
+  password: '',
+  confirmPassword: ''
+};
+
+const MyForm = () => {
+  const onSubmit = values = {};
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input placeholder='Name'/>
+      <br />
+      <input placeholder='Email'/>
+      <br />
+      <input placeholder='Password'/>
+      <br />
+      <input placeholder='Confirm password'/>
+      <br />
+      <input placeholder='Address'/>
+      <br />
+      <input placeholder='City'/>
+    </form>
   );
 }
 
-export default App;
+export default MyForm;
