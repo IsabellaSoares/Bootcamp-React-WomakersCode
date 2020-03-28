523605011f6961da7c96b7bdfe13868f781ed22e
Isabella <isabella.soares.28@gmail.com>
2020-03-28 13:17:40 -0300

    WIP - validation

--- formik-rooks-challenge/src/MyForm.js
+++ formik-rooks-challenge/src/MyForm.js
@@ -1,22 +1,27 @@
 import React, { useState, Fragment } from 'react';
-import { useFormik } from 'formik';
+import { useFormik, FormikProvider } from 'formik';
 import { Card, CardHeader, CardContent, Button } from '@material-ui/core';
 import './index.css';
 import { initialValues } from './initialValues';
 import { inputText as InputText } from './inputText';
 import SubmitSnackbar from './SubmitSnackbar';
+import * as Yup from 'yup'
 
-const MyFormFormikHooks = () => {
-	const [ open, setOpen ] = useState(false);	
-
-	const onSubmit = () => {
-		setOpen(true);		
-		setTimeout(() => formik.resetForm(), 6000);
-	};
-
+const MyForm = () => {
+	const [ open, setOpen ] = useState(false);
+	
 	const formik = useFormik({
 		initialValues,
-		onSubmit
+    validationSchema: Yup.object().shape({
+      name: Yup.string().required('field name is required'),
+      number: Yup.number().required('field number is required'),
+      cvc: Yup.string().required('field cvc is required'),
+      expiry: Yup.string().required('field expiry is required')
+    }),
+		onSubmit: values => {			
+			setOpen(true);		
+			setTimeout(() => formik.resetForm(), 6000);
+		}
 	});
 
 	return (
@@ -24,17 +29,19 @@ const MyFormFormikHooks = () => {
 			<Card className="my-form">
 				<CardHeader title="Form with Formik Hooks" />
 				<CardContent>
-					<form onSubmit={formik.handleSubmit}>
-						<InputText label="Name" {...formik.getFieldProps('name')} />
-						<InputText label="Email" {...formik.getFieldProps('email')} />
-						<InputText label="Password" {...formik.getFieldProps('password')} />
-						<InputText label="Confirm password" {...formik.getFieldProps('confirmPassword')} />
-						<InputText label="Address" {...formik.getFieldProps('address')} />
-						<InputText label="City" {...formik.getFieldProps('city')} />
-						<Button variant="outlined" color="secondary" type="submit" style={{ marginTop: '20px' }}>
-							Submit!
-						</Button>
-					</form>
+					<FormikProvider value={formik} >
+						<form onSubmit={formik.handleSubmit}>
+							<InputText label="Name" {...formik.getFieldProps('name')} />
+							<InputText label="Email" {...formik.getFieldProps('email')} />
+							<InputText label="Password" {...formik.getFieldProps('password')} />
+							<InputText label="Confirm password" {...formik.getFieldProps('confirmPassword')} />
+							<InputText label="Address" {...formik.getFieldProps('address')} />
+							<InputText label="City" {...formik.getFieldProps('city')} />
+							<Button variant="outlined" color="secondary" type="submit" style={{ marginTop: '20px' }}>
+								Submit!
+							</Button>
+						</form>
+					</FormikProvider>
 				</CardContent>
 			</Card>
 			<SubmitSnackbar open={open} setOpen={setOpen} formik={formik} />
@@ -42,4 +49,4 @@ const MyFormFormikHooks = () => {
 	);
 };
 
-export default MyFormFormikHooks;
+export default MyForm;
